4fa277a7c7ff160180efec72b6c052149e6cd31b
Fabiano S. Ramos <fabianosantosramos1978@gmail.com>
2025-07-02 07:41:29 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
-import { MissingParamError, InvalidParamError } from '../errors'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { badRequest } from '../helpers/http-helper'
 
 
@@ -14,14 +14,22 @@ export class SignUpController implements Controller {
 
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-    for(const field of requiredFields) {
+    try {
+      for(const field of requiredFields) {
         if(!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+          return badRequest(new MissingParamError(field))
+        } 
       }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if(!isValid) {
-      return badRequest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+        if(!isValid) {
+          return badRequest(new InvalidParamError('email'))
+      }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
