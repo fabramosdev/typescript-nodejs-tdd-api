2cc12e0bb7cd093a94a2c0bef7529c1dba8e7d9d
Fabiano S. Ramos <fabianosantosramos1978@gmail.com>
2025-07-03 09:15:22 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const config = require('./jest.config')
+const config = require('./jest.config.ts')
 config.testMatch = ['**/*.spec.ts']
 module.exports = config
